93b425d03a3ec2052315a945dd9eaa7df9eaa0ff
hugo98-py <hugo.vasquez@amsconsultores.cl>
2025-05-25 15:48:17 -0400

    Update main.py
    que soy wn, he tenido puros errores estupidos, nuevamente tenia mal puesta una triple coma jeje. 
    
    Intento 5
    Vamos chile

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Apr 19 19:37:45 2025
-"""
-
 @author: Hugo
+"""
 """ En este codigo se usa una versiona mas avanzada que la anterior, en donde se importa el archivo ecxel 
 con las BD de pruebas y se exporta el JSON con GET. """
 
