b40c5ca13e040c4da3c672cb6bd58faade34473d
hugo98-py <hugo.vasquez@amsconsultores.cl>
2025-06-02 23:15:04 -0400

    Update main.py
    Se agrega snippet para quitar tildes de la planilla.

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 import pandas as pd
 import os 
+import unicodedata
 
 app = FastAPI()
 
@@ -19,9 +20,21 @@ async def convert_excel_to_json():
     os.chdir(cwd)
     ruta_excel = "BD_FV_copia.xlsx"  
 
+    # Función para quitar tildes 
+    def quitar_tildes(texto):
+        if isinstance(texto, str):
+            return unicodedata.normalize('NFKD', texto).encode('ASCII', 'ignore').decode('utf-8')
+        return texto  # Si no es string, no lo modifica
+
+    # Leer el archivo original
+    df = pd.read_excel(ruta_excel, engine='openpyxl')
+
+    # Aplicar la función a todo el DataFrame (celdas y nombres de columnas)
+    df_sin_tildes = df.applymap(quitar_tildes)
+    df_sin_tildes.columns = [quitar_tildes(col) for col in df.columns]
+    
     try:
-        df = pd.read_excel(ruta_excel, engine='openpyxl')
-        df_clean = df.where(pd.notnull(df), None)  # <--- esta línea soluciona tu error
+        df_clean = df_sin_tildes.where(pd.notnull(df_sin_tildes), None)  # <--- esta línea maneja los datos nan y otros y los reemplaza por "None".
         datos_json = df_clean.to_dict(orient='records')
         return JSONResponse(content=datos_json)
     except Exception as e:
